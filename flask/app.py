5b4f6661f897e240df06e1fe13912bf8df9ee94b
root <root@nodyfang.localdomain>
2024-01-08 21:47:38 +0800

    update

--- flask/app.py
+++ flask/app.py
@@ -9,6 +9,10 @@ from linebot.models import MessageEvent, TextMessage, TextSendMessage
 line_bot_api = LineBotApi('XG2BHeGYWeIAd5H9zX6jM8tdH3dSm3m+vb5kl74qWl5uQpqST8N4m2DiScsIaYi61vjUM3fbjbLmrjkdq7CBJ1oPHh8x8Tlv3HQtP7fx5IuKuYzlr/Y6XIc1XXJeFPSGsjUlRjzky4J/YkmIMPFwQwdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('8c8eec4610aa229d3112cc45bb1c1b6f')
 
+@app.route("/helloworld")
+def hello():
+    return "Hello World!"
+
 @app.route("/callback", methods=['POST'])
 def callback():
     signature = request.headers['X-Line-Signature']
@@ -22,10 +26,6 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     line_bot_api.reply_message(event.reply_token,TextSendMessage(text=event.message.text))
-
-@app.route("/helloworld",method=['GET'])
-def hello():
-    return "Hello World!"
 
 if __name__ == '__main__':
     app.run(port=8000,host='0.0.0.0')
