288f98383bdae7eeb44b91d0ab22723e9ec35f8e
milliax <wei@sivir.pw>
2024-01-17 13:01:55 +0800

    update python code

--- flask/app.py
+++ flask/app.py
@@ -6,12 +6,21 @@ from linebot import  LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
+import requests
+
+user_key = "CWA-F87A3100-C712-40AA-B711-BFDB8BAE52B5" 
+doc_name = "F-C0032-001"
+
+url = 'https://opendata.cwa.gov.tw/fileapi/v1/opendataapi/%s?downloadType=WEB&format=JSON&Authorization=%s' % (doc_name,user_key)
+
 line_bot_api = LineBotApi('XG2BHeGYWeIAd5H9zX6jM8tdH3dSm3m+vb5kl74qWl5uQpqST8N4m2DiScsIaYi61vjUM3fbjbLmrjkdq7CBJ1oPHh8x8Tlv3HQtP7fx5IuKuYzlr/Y6XIc1XXJeFPSGsjUlRjzky4J/YkmIMPFwQwdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('8c8eec4610aa229d3112cc45bb1c1b6f')
 
+column = ['天氣狀況','最高溫','最低溫','舒適度','降雨機率(%)']
+
 @app.route("/helloworld",methods=['GET'])
-def hello():
-    return "Hello World!"
+def hendle_helloworld():
+    return "Hola"
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -25,7 +34,20 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(event.reply_token,TextSendMessage(text=event.message.text))
+    county = event.message.text
+    output = ""
+    
+    datas = requests.get(url).json()
+    
+    for data in datas['cwaopendata']['dataset']['location']:
+        if data['locationName'] == county.replace('台', '臺'):
+            for i in range(len(data['weatherElement'])):
+                output = output + column[i] + ":" + data['weatherElement'][i]['time'][0]['parameter']['parameterName'] + "\n"
+            break
+    else:
+        output = "請輸入正確的地名"
+    
+    line_bot_api.reply_message(event.reply_token,TextSendMessage(text=output))
 
 if __name__ == '__main__':
-    app.run(port=8000,host='0.0.0.0')
+    app.run(host='0.0.0.0', port=8000)
